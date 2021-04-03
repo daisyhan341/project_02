d73881a8a3611dd0c5b2bb571c978b8d116c525e
daisyhan341 <81408128+daisyhan341@users.noreply.github.com>
2021-04-03 17:39:55 +0900

    Create MemDTO.java

---
+++ java/member/MemDTO.java
@@ -0,0 +1,55 @@
+package member;
+
+//DTO(Data Transfer Object): JSP와 MYSQL이 서로 데이터를 전달하기 위한 단위
+public class MemDTO {
+	String mem_id;
+	String mem_pw;
+	String mem_nick;
+	String mem_email;
+	String mem_phone;
+	String mem_birth;
+	String mem_group;
+	
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_nick() {
+		return mem_nick;
+	}
+	public void setMem_nick(String mem_nick) {
+		this.mem_nick = mem_nick;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_phone() {
+		return mem_phone;
+	}
+	public void setMem_phone(String mem_phone) {
+		this.mem_phone = mem_phone;
+	}
+	public String getMem_birth() {
+		return mem_birth;
+	}
+	public void setMem_birth(String mem_birth) {
+		this.mem_birth = mem_birth;
+	}
+	public String getMem_group() {
+		return mem_group;
+	}
+	public void setMem_group(String mem_group) {
+		this.mem_group = mem_group;
+	}
+}
