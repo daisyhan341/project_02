0c540c32b27de495edc1516d91f6c2929b2bb3ad
daisyhan341 <81408128+daisyhan341@users.noreply.github.com>
2021-04-05 00:08:02 +0900

    Update ddl.sql

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -1,1 +1,3 @@
-SELECT * FROM project_02.member;
+ALTER DATABASE project_02 DEFAULT CHARACTER SET utf8;
+ALTER TABLE project_02.member ADD mem_group VARCHAR(10);
+ALTER TABLE project_02.member ADD join_date DATETIME;
